f143639efc8ae97f5d7876e102327e0409446178
LAPTOP-6RTVO9CN\yugal <yugalavhte1707@gmail.com>
2022-08-19 11:08:58 +0530

    Python first program (Yugandhar)

---
+++ Demo.py
@@ -0,0 +1,37 @@
+print("Hello World!")
+
+a = 10
+name = "python"
+print(a)
+print(name)
+b = c = d = 12
+print(b)
+print(c)
+print(d)
+
+e,f,g=14,15,16
+print(e)
+print(f)
+print(g)
+
+_nav = "Yugandhar"
+print(_nav)
+
+_h = 4
+print(_h)
+
+print(type(_h))
+print(type(name))
+
+i = 4.5
+print(i)
+print(type(i))
+
+arr = [12,15,17,19] #list type must be in square brackets
+print(type(arr))  
+
+w,x,y,z = arr
+print(w)
+print(x)
+print(y)
+print(z)
