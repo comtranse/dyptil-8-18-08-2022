860881a0a4239d6afb737eeeee83106484a21902
PRATHAM\Lenovo <prathampatil4249@gmail.com>
2022-08-22 11:49:48 +0530

    tokes(operator)

---
+++ tokensByPPatil.py
@@ -0,0 +1,85 @@
+# # additon
+# a=5
+# b=7
+# print(a+b)
+# print(6+8)
+  
+#   #assignmnet operator
+# x=10
+# print(x)
+# x +=12
+# print(x)
+# y=20
+# print(y)
+# y -=10
+# print(y)
+
+# a=10 
+# b=4
+# print('a & b',a & b)
+
+w=10
+d=-5
+print("w>>1 =",  w>>1)
+
+ #comprose
+c=9
+print(c == 12)
+print(c == 55)
+print(c != 3)
+print(c >= 55)
+print(c <= 45)
+
+#logical
+a= 14
+b= 12
+print("a and b" ,a==14  and b>=12)
+print("a or b" ,a==14  or b>=5)
+print("not a", not a ==11)
+#identiy operator
+id(9)
+print(id)    #<built-in function id>
+print(id(9))     #2586105872880
+print(id("pratham"))  #2586107253232
+print(1 is 1)
+print(1 is 2)
+print(1 is not 5)
+#membership 
+arr=[1,2,5,6,8]
+print(arr)
+print(5 in arr)
+print(2 not in arr)
+
+# identifiers
+#x= 10
+#y= 5
+#print(X)
+#print(x)
+# literal
+str ="name"
+print(str)
+str2='''abc
+xyz
+mnq'''
+print(str2)
+num=19
+print(num)
+f=5.5
+print(f)
+#copmlex literal
+z=78j
+print(z)
+#boolean
+password=225533
+print(password==112244)
+print(password==225533)
+
+#find the gratherthan numbers
+a=5
+b=7
+print(a>b)
+print(b>a)
+if(a>b):
+    print("is grater")
+else:
+        print("is lessthan")
