186b8da2fbc0bc66211dec45f18c5df18b19cdc8
PRATHAM\Lenovo <prathampatil4249@gmail.com>
2022-08-23 12:02:13 +0530

    tokes,datatype

---
+++ datatype.py
@@ -0,0 +1,45 @@
+
+
+
+var1 = "Hello PRATHAM"
+var2 =' Hello " PRATHAM"'
+print(var1)
+print(var2)
+
+var3 = "My name is 'Pratham' "
+print(var3)
+var4 = "\bMy name is\n \"Pratham\""
+print(var4)
+
+var5 ="welcome  to EL DORDO"
+print(len(var5)) 
+print(var5.upper())
+print(var5.lower())
+
+print(var5.strip())
+print(var5.count("e"))
+print(var5.replace("DORDO","KGF"))
+
+var6="world"
+print(var6.center(9))
+
+var3=var6+var1
+print(var3)
+
+var7="name{}"
+var8=10
+print(var7.format(var8))
+
+var9 ="TATA"
+var10 = 9
+print("my name is {} and roll number is {} ".format(var9,var10))
+var11 =15
+print(float(var11))
+print(int(var11))
+
+var12=22.19j
+print(float(var12))
+print(int(var12))
+
+
+
---
+++ tuple.py
@@ -0,0 +1,46 @@
+
+
+
+var1=(12,11,33,44)
+print(var1)
+print(var1[2])
+
+var2=("Pratham",4249,9,(6567),[12,22])
+print(var2)
+print(var2[3])
+print(var2[-1])
+print(var2[1:3])
+
+
+#var3=([1,55,99,22][2,5,88,99])
+#print(var3[0][0])
+#print(var3[0][1])
+#print(var3[0][2])
+#print(var3[1][0])
+#print(var3[1][1])
+
+var4 =("PP","PP")
+var5 =(("PP ")*5)
+print(var5)
+print(var4)
+ 
+a=("DYP",99,"Pratham")
+b=("KIT",55,"Sai")
+(college,rollno,name)=a
+(clg,rno,nm)=b
+print(college)
+print(rollno)
+print(name)
+
+
+var6 =list(var2)
+print(var6)
+var6[1]="Hello"
+x= tuple(var6)
+print(x)
+
+
+
+
+
+
