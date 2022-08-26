d82c3c3143f046607195c3a563144bb4d2f7b0a2
LAPTOP-LRD21285\Soham <valanjubapu2001@gmail.com>
2022-08-26 10:58:13 +0530

    Array (Bapu Valanju)

---
+++ new_Array.py
@@ -0,0 +1,20 @@
+import array as arr
+from operator import index
+a=arr.array('B',[1,2,3,4])
+# print(a[1])
+# b=arr.array('B'['abc','xyz'])
+# print(b[1])
+# a[2]=30
+# print(a)
+b=[2,3,5,4,6,8]
+# for x in b:
+#     print(x)
+#find prime number by using for and while loop
+for x in b:
+    if x==3:
+        break
+    print(x)
+# for x in b:
+#      if x==3:
+#         continue
+#     print(x)
