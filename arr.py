b0b03f51ad7e91ae92ca93d849297e6f6ae4ea88
PRATHAM\Lenovo <prathampatil4249@gmail.com>
2022-08-26 21:47:11 +0530

    Array and loop (for loop )

---
+++ arr.py
@@ -0,0 +1,10 @@
+import array as arr
+x= arr.array("L",[22,55,85,65])
+print(x[2])
+
+
+
+x[3]=99
+print(x)
+
+
---
+++ loop.py
@@ -0,0 +1,28 @@
+
+
+
+li=[12,25,45,63,97]
+for i in li:
+    print(i)
+    #task prime numbers for loop
+for i in range(1,11):
+      count=0
+      for j in range(2,i):
+        if i%j==0:
+            count=count+1
+      if count==0:
+                print(i)
+      else:
+                count=0
+
+    #  while loop
+
+   
+
+for i in li:
+    if(i==25):
+        continue
+    print("found")
+        
+
+      
