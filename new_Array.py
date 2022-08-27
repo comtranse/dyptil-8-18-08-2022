72ad8ebdc47530595f19e8fdb490e31bb21c1624
LAPTOP-LRD21285\Soham <valanjubapu2001@gmail.com>
2022-08-27 10:56:27 +0530

    functions and array(Bapu Valanju)

--- new_Array.py
+++ new_Array.py
@@ -1,5 +1,6 @@
 import array as arr
 from operator import index
+from pickle import TRUE
 a=arr.array('B',[1,2,3,4])
 # print(a[1])
 # b=arr.array('B'['abc','xyz'])
@@ -18,3 +19,52 @@ for x in b:
 #      if x==3:
 #         continue
 #     print(x)
+
+# while loop
+j=1
+while (j<=20):
+    print(j)
+    j=j+1
+# amstrong number
+i=1
+k=2
+
+# while(i<=10):
+#     count=0
+
+#     while(k<1):
+#         if(i%k)==0:
+#             count=count+1
+#         i=i+1
+#         k=k+1
+#     if count==0:
+#         print(i)
+#     else:
+#         count=0
+#     # int(input(""))
+    
+# a=10
+# b=20
+# if (a<b):
+#    print(a,"is greater value")
+# elif(a==b):
+#     print(b)
+# else:
+#     print(b,"is greater value")
+
+
+
+# c,d=map(int,input("enter two numbers:").split())
+# if (c%2==0 and d%2==0):
+#     print(c,d,"both numbers are even")
+# elif(c%2==0 and d%2!=0):
+#     print(c,"is even",d,"is odd")
+# else:
+#     print(d,"is even",c,"is odd")
+
+def demo(x,y):
+    print("the addition is :",x+y)
+    print("the substraction is:",x-y)
+
+demo(20,45)
+
