2f85ee3c62909f9485154e978fdc5bc8d48a3fa5
DESKTOP-E05QMB3\Lenovo <pranavmane327@gmail.com>
2022-08-27 10:55:23 +0530

    loops and func

---
+++ while_func.py
@@ -0,0 +1,57 @@
+i=0
+while(i<10):
+    print(i)
+    i+=1
+
+# B=10
+# r=s=0
+# temp=B
+# while(B>0):
+#     r=B%10
+#     s=s+(r*r*r)
+#     B=B/10
+# if(temp==s):
+#     print("Armstrong")
+# else:
+#     print("Not Armstrong")
+
+a=30
+b=20
+# if(a<b):
+#     print("b is max")
+# else:
+#     print("a is max")
+
+# c=int(input(""))
+# d=int(input(""))
+# e=int(input(""))
+# if(c<d):
+#     if(c<e):
+#         print("a is smaller")
+# elif(d<e):
+#     print("b is smaller")
+# else:
+#     print("c is smaller")
+
+# f=int(input(""))
+# if(f%2==0):
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+def fstfun():
+    print("abc")
+fstfun()
+
+def fun(y,z):
+    print(y,z)
+fun(2,3)
+
+def fun1(w,x):
+    print(w+x)
+fun1("66","yy")
+
+def add():
+    print("sum =",a+b)
+add()
