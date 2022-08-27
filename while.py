47ffdef1d4a7a8dcd957f4cbed2d6537cdf8ae52
PRATHAM\Lenovo <prathampatil4249@gmail.com>
2022-08-27 10:51:10 +0530

    while loop  , function,conditional

---
+++ while.py
@@ -0,0 +1,47 @@
+#while loop
+var =1
+while(var<20):
+     print(var)
+     var=var+1
+
+
+
+            #conditional statement
+# print("enter two numbrs")            
+# a=int (input())
+# b=int (input())
+# if(a<b):
+#     print("{} is greather".format(a))
+# else:
+#     print("{} is lessthan".format(b))
+    #task get 3 numbers and 2 garther than even or odd
+# print("Enetr any three numbers")
+# num1=int(input())
+# num2=int(input())
+# num3=int(input())
+# if(num1>num2 and num1>num3):
+#         print("{} is greter".format(num1))
+# elif(num2>num1 and num2>num3):
+#         print("{} is greter" .format(num2))
+# else:
+#             print("{} is grater".format(num3))
+
+    #function
+# def fun():
+#     print("abc")
+# fun()
+
+# def fun2(x):
+#     print("value of x is {}",format(x))
+
+
+# fun2(10)   
+def op(a,b):
+    print("Addtion is {}".format(a+b))
+    print("substraction is {}".format(a-b))
+    print("multi is {}".format(a*b))
+    print("Division is {}".format(a/b))
+
+op(10,25)    
+
+
