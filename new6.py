438ff6d5f09992b866730db2f1778c646e6d3dd8
LAPTOP-LRD21285\Soham <valanjubapu2001@gmail.com>
2022-09-06 11:29:21 +0530

    concepts of oop(Bapu Valanju)

---
+++ new6.py
@@ -0,0 +1,43 @@
+# import camelcase as camel
+# c=camel.CamelCase()
+# txt="hello world"
+# print(c.hump(txt))
+
+# try:
+#     k=20
+#     j="hello"
+#     a=k/j
+#     print(a)
+# except ZeroDivisionError as e:
+#     print("true")
+# except NameError as u:
+#     print("false")
+# except TypeError as h:
+#     print("ok")
+# finally:
+#     print("executed")
+
+class comtrance:
+    name="soham"
+    def college(self):
+        print("hi")
+# print(comtrance.name)
+# comtrance.college()
+obj=comtrance()
+obj.college()
+     
+   
+    #  print(comtrance.name)
+    #  comtrance.college()
+    #   print(name)
+class new:
+    def soham():
+        name="hello"
+        print(name)
+    def address():
+        add="malkapur , karad"
+        print(add)
+obj=new
+obj.soham()
+obj.address()
+
